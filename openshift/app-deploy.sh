3e171ec8b489944b42b8d777484438e5b1065347
Jim Minter <jminter@redhat.com>
2015-07-03 11:45:35 +0100

    fix connection strings

--- openshift/app-deploy.sh
+++ openshift/app-deploy.sh
@@ -65,19 +65,9 @@ items:
           - name: JAVA_OPTS
             value: "-server -XX:+UseCompressedOops -verbose:gc -Xloggc:/opt/eap/standalone/log/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=3M -XX:-TraceClassUnloading -Xms128m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.logmanager -Djava.awt.headless=true -Djboss.modules.policy-permissions=true -Xbootclasspath/p:/opt/eap/jboss-modules.jar:/opt/eap/modules/system/layers/base/org/jboss/logmanager/main/jboss-logmanager-1.5.4.Final-redhat-1.jar:/opt/eap/modules/system/layers/base/org/jboss/logmanager/ext/main/javax.json-1.0.4.jar:/opt/eap/modules/system/layers/base/org/jboss/logmanager/ext/main/jboss-logmanager-ext-1.0.0.Alpha2-redhat-1.jar -Djava.util.logging.manager=org.jboss.logmanager.LogManager -javaagent:/opt/eap/jolokia.jar=port=8778,host=0.0.0.0,discoveryEnabled=false"
           - name: DB_SERVICE_PREFIX_MAPPING
-            value: mysql
-          - name: MYSQL_SERVICE_HOST
-            value: mysql
-          - name: MYSQL_SERVICE_PORT
-            value: "3306"
-          - name: mysql_JNDI
-            value: "java:jboss/datasources/MySqlDS"
-          - name: mysql_USERNAME
-            value: "$MYSQL_USER"
-          - name: mysql_PASSWORD
-            value: "$MYSQL_PASS"
-          - name: mysql_DATABASE
-            value: "$MYSQL_DATABASE"
+            value: ignore
+          - name: datasources
+            value: "<datasource jndi-name=\"java:jboss/datasources/MySqlDS\" pool-name=\"MySqlDS\" enabled=\"true\" use-java-context=\"true\"><connection-url>jdbc:mysql://mysql:3306/$MYSQL_DATABASE</connection-url><driver>mysql</driver><security><user-name>$MYSQL_USER</user-name><password>$MYSQL_PASS</password></security></datasource>"
   
 - kind: Service
   apiVersion: v1
